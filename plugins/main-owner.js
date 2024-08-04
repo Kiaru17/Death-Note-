8551487fd4c43f5336af4d4a825899efb894de42
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <Roldanrojas801@gmail.com>
2024-08-04 14:07:13 -0300

    Actualizar main-owner.js

--- plugins/main-owner.js
+++ plugins/main-owner.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Kiara;;\nFN:Kiara\nORG:JTxꜱ\nTITLE:\nitem1.TEL;waid=5493625494354⁩:5493625494354⁩ \nitem1.X-ABLabel:Kiara\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Kiara\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Kiara', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;JTxꜱ⁩;;\nFN:JTxꜱ\nORG:JTxꜱ\nTITLE:\nitem1.TEL;waid=5491126788746:5493625494354\nitem1.X-ABLabel:JTxꜱ\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:JTxꜱ\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'JTxꜱ', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
