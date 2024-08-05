d4510e7dc0d660c60943fabc90e225a6be1c0d85
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <Roldanrojas801@gmail.com>
2024-08-04 22:07:38 -0300

    Actualizar main-owner.js

--- plugins/main-owner.js
+++ plugins/main-owner.js
@@ -1,10 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;JTxꜱ⁩;;\nFN:⋱
-*⏤͟͟͞͞IKiara*  ⃖  ⃯ ࿉۟࣪࣪᭝\nORG:⋱
-*⏤͟͟͞͞IKiara*  ⃖  ⃯ ࿉۟࣪࣪᭝\nTITLE:\nitem1.TEL;waid=5493625494354:5493625494354\nitem1.X-ABLabel:⋱
-*⏤͟͟͞͞IKiara*  ⃖  ⃯ ࿉۟࣪࣪᭝nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:JTxꜱ\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: '⋱
-*⏤͟͟͞͞IKiara*  ⃖  ⃯ ࿉۟࣪࣪᭝', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;JTxꜱ⁩;;\nFN:⏤͟͟͞͞IKiara\nORG:⏤͟͟͞͞IKiara\nTITLE:\nitem1.TEL;waid=5493625494354:5493625494354\nitem1.X-ABLabel:⏤͟͟͞͞IKiaranX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:⏤͟͟͞͞IKiara\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: '⏤͟͟͞͞IKiara', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
